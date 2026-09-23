98e62a67bda69b0a8f7359f7f3795343eacb5819
Josip Kelecic <kelecicj@gmail.com>
2026-09-23 14:34:55 +0200

    vontarx4: Switch proprietary blobs to the stock ah218 dump
    Default to Amlogic/ah218. Keep ohm-pinned DRM libs and jarvis-pinned
    Netflix xmls. Drop Amlogic KeyMint blobs and ohm TEE applets that this
    board does not use.
    
    Change-Id: I5addfaef5c12c3bfdc311be7281741d2d6518337

--- extract-files.py
+++ extract-files.py
@@ -4,11 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.fixups_blob import (
-    blob_fixup,
-    blob_fixups_user_type,
-)
-
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -18,22 +13,9 @@ namespace_imports = [
     'vendor/amlogic/ne-common',
 ]
 
-blob_fixups: blob_fixups_user_type = {
-    'vendor/bin/hw/android.hardware.security.keymint-service.amlogic': blob_fixup()
-        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_akeymint_utils.so')
-        .replace_needed('libkeymint.so', 'libakeymint.so')
-        .replace_needed('libkeymint_remote_prov_support.so', 'libakeymint_remote_prov_support.so'),
-    'vendor/lib/libakeymint.so': blob_fixup()
-        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_akeymint_utils.so'),
-    'vendor/lib/libakeymint_remote_prov_support.so': blob_fixup()
-         .replace_needed('libcppcose_rkp.so', 'libacppcose_rkp.so')
-         .replace_needed('libbase.so', 'libbase-v33.so'),
-}  # fmt: skip
-
 module = ExtractUtilsModule(
     'vontarx4',
     'amlogic',
-    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
     add_firmware_proprietary_file=True,
 )
